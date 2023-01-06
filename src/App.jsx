4c3fe064deee9c5e630faed8945ffe2d8e2d20c3
JOSE VARGAS¡¡ <joseroberto-vargas@hotmail.com>
2023-01-05 20:55:02 -0500

    terminadas las locations y el diseño del banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import './App.css'
 import axios from 'axios'
 import Location from './components/Location'
 
+
 function App() {
 
 
   return (
     <div className="App">
+      <div className='header-container'>
+        {/* container para la imagen de fondo del banner */}
+      </div>
       <Location/>
      
       
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -3,48 +3,62 @@ import axios from 'axios'
 
 
 const Location = () => {
-    const [location, setLocation]= useState({})
-    const[searchIdLocation, setSearchIdLocation]=useState(" ")
-    
-    
-    useEffect(()=>{
-      const randomId = Math.floor(Math.random()*126) + 1;
-      axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
-      .then(res=>setLocation(res.data))
-    
-     
-    
-    },[]);
-    console.log(location);
-    
+    const [location, setLocation] = useState({})
+    const [searchIdLocation, setSearchIdLocation] = useState(" ")
 
-    const searchLocation =()=>{
-      axios.get(`https://rickandmortyapi.com/api/location/${searchIdLocation || "1"}`)
-      .then(res=>setLocation(res.data))
-    
+
+    useEffect(() => {
+        const randomId = Math.floor(Math.random() * 126) + 1;
+        axios.get(`https://rickandmortyapi.com/api/location/${randomId}`)
+            .then(res => setLocation(res.data))
+
+
+
+    }, []);
+    console.log(location);
+
+
+    const searchLocation = () => {
+        axios.get(`https://rickandmortyapi.com/api/location/${searchIdLocation || "1"}`)
+            .then(res => setLocation(res.data));
+        if (searchIdLocation > 126) {
+            alert("Only 126 Locations")
+        }
     }
     return (
-        <div>
-            <h1>Rick y Morthy</h1>
-      <input
-        type="text"
-        placeholder="type a location for id"
-        value={searchIdLocation}
-        onChange={(e) => setSearchIdLocation(e.target.value)}
-      />
-      <button onClick={searchLocation}>Search location</button>
-     <h2>{location.name}</h2>
-     <ul>
-      {
-        location.residents?.map(resident =>(
-          <li key={resident}>
-            {resident}
-          </li>
-        ))
-      }
+        <div className='container-location'>
+            <h1 className='title'>Rick and Morty App</h1>
+            <div className='container-search'>
 
-     </ul>
-            
+                <input
+                    type="text"
+                    placeholder="Type a location for id"
+                    value={searchIdLocation}
+                    onChange={(e) => setSearchIdLocation(e.target.value)}
+                />
+                <button onClick={searchLocation}>Search location</button>
+
+            </div>
+            <h2 className='title-location'>{location.name}</h2>
+            <div className='location-info'>
+                <h2><b>Type:</b>  {location.type}</h2>
+                <h2><b>Dimension:</b>  {location.dimension}</h2>
+                <h2><b>Population:</b>  {location.residents?.length}</h2>
+            </div>
+            <div className='list-residents'>
+                <ul>
+                    {
+                        location.residents?.map(resident => (
+                            <li key={resident}>
+                                {resident}
+                            </li>
+                        ))
+                    }
+
+                </ul>
+            </div>
+
+
         </div>
     );
 };

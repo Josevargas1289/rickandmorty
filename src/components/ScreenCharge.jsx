55aebe4f0453f96e6868d61c366cb4314c6f3615
JOSE VARGAS¡¡ <joseroberto-vargas@hotmail.com>
2023-01-06 22:32:42 -0500

    agregando pantalla de carga

--- src/components/ScreenCharge.jsx
+++ src/components/ScreenCharge.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from 'react'
+import rick from "../assets/img/rick.jpg"
+
 
 const ScreenCharge = () => {
     return (
         <div className='screen-container'>
             <div className='screen'>
-                <img src="./src/assets/rick.jpg" alt="img" />
+                <img src={rick} alt="img" />
                 <h1 className='title-screen'>Cargando. . . .</h1>
                 <h1 className='screnn-welcome'>Bienvenido A: Rick <br /> And Morty APP</h1>
             </div>
